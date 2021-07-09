8f52e809056327e0eabb20406e49c519efbc5743
aforce-kanri <aforce-kanri@DESKTOP-22PMEMU>
2021-07-09 14:45:55 +0900

    commit

--- src/jp/co/aforce/logaction/InsertAction.java
+++ src/jp/co/aforce/logaction/InsertAction.java
@@ -1,31 +1,31 @@
-package jp.co.aforce.logaction;
-
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import jp.co.aforce.bean.Members;
-import jp.co.aforce.dao.MembersDAO;
-import jp.co.aforce.tool.Action;
-
-public class InsertAction extends Action{
-	public String execute(
-			HttpServletRequest request,HttpServletResponse response
-		)throws Exception{
-			
-			String login=request.getParameter("login");
-			String password=request.getParameter("password");
-			
-			Members p=new Members();
-			p.setLogin(login);
-			p.setPassword(password);
-			MembersDAO dao=new MembersDAO();
-			dao.insert(p);
-			
-//			List<Members> list=dao.search("");
-//			request.setAttribute("list", list);
+//package jp.co.aforce.logaction;
+//
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import jp.co.aforce.bean.Members;
+//import jp.co.aforce.dao.MembersDAO;
+//import jp.co.aforce.tool.Action;
+//
+//public class InsertAction extends Action{
+//	public String execute(
+//			HttpServletRequest request,HttpServletResponse response
+//		)throws Exception{
+//			
+//			String login=request.getParameter("login");
+//			String password=request.getParameter("password");
+//			
+//			Members p=new Members();
+//			p.setLogin(login);
+//			p.setPassword(password);
+//			MembersDAO dao=new MembersDAO();
+//			dao.insert(p);
+//			
+////			List<Members> list=dao.search("");
+////			request.setAttribute("list", list);
 ////			
-			return ;
-		}
-	
-}
+//			return ;
+//		}
+//	
+//}
